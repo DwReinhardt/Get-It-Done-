e795c6484219f6969d9d21d8a310c167af092ac2
Duane Reinhardt <dwreinhardt75@outlook.com>
2018-10-02 23:31:07 -0400

    ....

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:launchcode101@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
 
-db = SQLAlchemy(app) #creates the database object
 
-class Task(db.model):
-    id = db.Column(db.Integer, primary_key=True)
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
 
     def __init__(self, name):
