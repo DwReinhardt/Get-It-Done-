a48efa634a94200d388129ea734525e3d185c15d
Duane Reinhardt <dwreinhardt75@outlook.com>
2018-10-03 20:23:53 -0400

    added a handler to delete item from db

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:launchcode101@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
 
+db = SQLAlchemy(app)
 
 class Task(db.Model):
 
@@ -15,16 +16,27 @@ class Task(db.Model):
     def __init__(self, name):
         self.name = name
 
-tasks = []
-
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
-        task = request.form['task']
-        tasks.append(task)
+        task_name = request.form['task']
+        new_task = Task(task_name)
+        db.session.add(new_task)
+        db.session.commit()
 
+    tasks = Task.query.all()
     return render_template('todos.html', title="Get It Done!", tasks=tasks)
+
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id']) #html returns as a string so, convert to int
+    task = Task.query.get(task_id) #request from db the item
+    db.session.delete(task) # tell db to delete
+    db.session.commit() #commit to db
+
+    return redirect('/') #return user to a view
 
 if __name__ == '__main__':
     app.run()
